04b291e4ef181c2ead8e6836119e759917b38be6
ismad <ismadiez29@gmail.com>
2020-12-28 21:09:57 +0100

    Pre branch commit

---
+++ app/src/main/java/com/example/notas/adapters/NotesAdapter.java
@@ -0,0 +1,15 @@
+package com.example.notas.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotesAdapter {
+
+    static class NoteViewHolder extends RecyclerView.ViewHolder{
+        NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/notas/adapters/adapter.kt
@@ -0,0 +1,10 @@
+package com.example.notas.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class adapter {
+
+    internal class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+}
